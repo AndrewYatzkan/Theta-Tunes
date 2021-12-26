a2c2597b02a8547ec29ab9dfe5d67f2571159a7f
AndrewYatzkan <andrew@yatzkan.com>
2021-12-26 14:47:11 -0600

    checks for duplicate URIs and checks that track actually exists

--- spotify.py
+++ spotify.py
@@ -1,14 +1,26 @@
 # to-do: add error handling
-
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
+import logging
+
+# silences errors when trackExists returns false
+logger = logging.getLogger("spotipy")
+logger.setLevel(logging.CRITICAL)
 
 load_dotenv() # load environment variables (for authentication flow)
 
 scope = "playlist-modify-private" # set scope for spotify api
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope)) # create spotipy object
+
+# to-do: temporarily silence stdout when sp.track is called
+def trackExists(uri):
+	try:
+		sp.track(uri)
+		return True
+	except spotipy.exceptions.SpotifyException:
+		return False
 
 # gets the URIs of tracks already on the playlist
 def loadURIs(playlist):
@@ -24,16 +36,16 @@ def loadURIs(playlist):
 	return results
 
 # adds the list of URIs to the playlist
-def addTracks(URIs, playlist="0Ub5rHeNVKSoSaJocpKBsa"):
+def addTracks(URIs, playlist):
 	allTrackURIs = loadURIs(playlist) # fetch tracks so we don't add a duplicate
 	print("Fetched track URIs")
-	newTracks = list(filter(lambda uri : uri not in allTrackURIs, URIs))
+	newTracks = list(filter(lambda uri : uri not in allTrackURIs and trackExists(uri), list(set(URIs))))
+	# for uri in newTracks:
+		# sp.track(uri)
 	if newTracks:
 		print("Adding %d new track(s)" % len(newTracks))
 		sp.playlist_add_items(playlist, newTracks)
 		print("Track(s) successfully added")
+		return len(newTracks)
 	else:
 		print("No new tracks")
-
-# to-do: load URIs that we want to be added
-addTracks(["spotify:track:1lwvJQGhdq6Kyr4BBquf23", "spotify:track:0HsRWeoq9eME1Vgc8Evfhq"])
