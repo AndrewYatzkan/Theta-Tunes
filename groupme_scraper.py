36f06b56f6e73b0ec86cc495acf67fbacce9a43a
AndrewYatzkan <andrew@yatzkan.com>
2021-12-26 12:17:43 -0600

    removed spotify api code from groupme portion. spotify.py already handles uri validation

--- groupme_scraper.py
+++ groupme_scraper.py
@@ -1,25 +1,5 @@
 from groupy import Client
-import requests
-import json
 import re
-
-
-AUTH_URL = 'https://accounts.spotify.com/api/token'
-
-auth_response = requests.post(AUTH_URL, {
-    'grant_type': 'client_credentials',
-    'client_id': spot_id,
-    'client_secret': spot_secret,
-})
-
-# convert the response to JSON
-auth_response_data = auth_response.json()
-
-# save the access token
-access_token = auth_response_data['access_token']
-headers = {
-    'Authorization': 'Bearer {token}'.format(token=access_token)
-}
 
 client = Client.from_token(token)
 messages = None
